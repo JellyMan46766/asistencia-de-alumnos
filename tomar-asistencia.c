02387921c68c9ab4dd7a05b0063079cbd16a43a2
Jelly_Man <castillosant19@protonmail.com>
2025-09-19 11:34:16 -0600

    functions prototipes

--- tomar-asistencia.c
+++ tomar-asistencia.c
@@ -1,5 +1,5 @@
 typedef struct {
-  int *present; // 1 - present, 0 - no present
+  int *present; // 1 - present, 0 - not present
   int id;
   char *name;
 } TStudent;
@@ -8,3 +8,14 @@ typedef struct {
   TStudent student;
   struct TNode *next;
 } TNode;
+
+void printMenu();
+int readNStudents();
+void readStudentsData();
+TNode *createNode(TStudent student);
+TNode *insertStart(TNode *head, TStudent student);
+
+void takeAttendance(TNode *head);
+void printDayAttendance(TNode *head);
+void printWeekAttendance(TNode *head);
+void advanceDay();
