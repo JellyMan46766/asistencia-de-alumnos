ae4ae9e23f66041b89fda6e0a6fb127dd10db0ba
Jelly_Man <castillosant19@protonmail.com>
2025-09-19 16:08:51 -0600

    more function definitions

--- tomar-asistencia.c
+++ tomar-asistencia.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define OFFSET 1
+#define WEEK_DAYS 5
 
 typedef struct {
-  int *present; // 1 - present, 0 - not present
+  int *attendance; // 1 - present, 0 - not present
   int id;
   char *name;
 } TStudent;
@@ -14,12 +16,12 @@ typedef struct {
   struct TNode *next;
 } TNode;
 
-void menu();
 void welcome();
 int readNStudents();
 void readStudentsData(int n_students);
 TNode *createNode(TStudent student);
 TNode *insertStart(TNode *head, TStudent student);
+void printMenu();
 
 void takeAttendance(TNode *head);
 void printDayAttendance(TNode *head);
@@ -28,17 +30,13 @@ void advanceDay();
 
 int main()
 {
-  menu();
-
-  return 0;
-}
-
-void menu()
-{
   int n_students;
   welcome();
   n_students = readNStudents();
   readStudentsData(n_students);
+  printMenu();
+
+  return 0;
 }
 
 void welcome()
@@ -65,7 +63,7 @@ int readNStudents()
 
 void readStudentsData(int n_students)
 {
-  TNode *head;
+  TNode *head = NULL;
   TStudent current_student;
   printf("X-------------------------------X\n");
   for (int i = 0; i < n_students; i++) {
@@ -75,4 +73,33 @@ void readStudentsData(int n_students)
     scanf("%[^\n]", current_student.name);
     head = insertStart(head, current_student);
   }
+}
+
+TNode *insertStart(TNode *head, TStudent current_student)
+{
+  TNode *new = createNode(current_student);
+  new->next = head;
+  return new;
+}
+
+TNode *createNode(TStudent current_student)
+{
+  TNode *new = (TNode *)malloc(sizeof(TNode));
+  if (new == NULL) {
+    printf("Error al asignar memoria\n");
+    return NULL;
+  }
+  new->student.id = current_student.id;
+  new->student.name =
+      (char *)malloc(strlen(current_student.name) * sizeof(char));
+  if (new->student.name == NULL) {
+    printf("Error al asignar memoria\n");
+    return NULL;
+  }
+  new->student.attendance = (int *)malloc(WEEK_DAYS * sizeof(int));
+  if (new->student.attendance == NULL) {
+    printf("Error al asignar memoria\n");
+    return NULL;
+  }
+  return new;
 }
